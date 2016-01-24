cd7b9eb2fcfcb4db1f67d12f33c96bdf16163b67
AlexandrYou <mail@mail.ru>
2016-01-24 12:20:01 +0300

    newproj

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsFormsApplication1
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Требуется переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Обязательный метод для поддержки конструктора - не изменяйте
+    /// содержимое данного метода при помощи редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.кодироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.декодироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.переводСтрокиMACToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(248, 58);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 111);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(248, 187);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(174, 111);
+            this.textBox2.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.кодироватьToolStripMenuItem,
+            this.декодироватьToolStripMenuItem,
+            this.переводСтрокиMACToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(706, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // кодироватьToolStripMenuItem
+            // 
+            this.кодироватьToolStripMenuItem.Name = "кодироватьToolStripMenuItem";
+            this.кодироватьToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.кодироватьToolStripMenuItem.Text = "Кодировать";
+            this.кодироватьToolStripMenuItem.Click += new System.EventHandler(this.кодироватьToolStripMenuItem_Click);
+            // 
+            // декодироватьToolStripMenuItem
+            // 
+            this.декодироватьToolStripMenuItem.Name = "декодироватьToolStripMenuItem";
+            this.декодироватьToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.декодироватьToolStripMenuItem.Text = "Декодировать";
+            this.декодироватьToolStripMenuItem.Click += new System.EventHandler(this.декодироватьToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(248, 320);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(174, 101);
+            this.textBox3.TabIndex = 4;
+            // 
+            // переводСтрокиMACToolStripMenuItem
+            // 
+            this.переводСтрокиMACToolStripMenuItem.Name = "переводСтрокиMACToolStripMenuItem";
+            this.переводСтрокиMACToolStripMenuItem.Size = new System.Drawing.Size(137, 20);
+            this.переводСтрокиMACToolStripMenuItem.Text = "Перевод строки MAC";
+            this.переводСтрокиMACToolStripMenuItem.Click += new System.EventHandler(this.переводСтрокиMACToolStripMenuItem_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(462, 58);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(148, 111);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(462, 187);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(148, 111);
+            this.textBox5.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 499);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+    private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    private System.Windows.Forms.ToolStripMenuItem кодироватьToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem декодироватьToolStripMenuItem;
+    private System.Windows.Forms.TextBox textBox3;
+    private System.Windows.Forms.ToolStripMenuItem переводСтрокиMACToolStripMenuItem;
+    private System.Windows.Forms.TextBox textBox4;
+    private System.Windows.Forms.TextBox textBox5;
+
+  }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      if (openFileDialog1.ShowDialog() == DialogResult.OK)
+      {
+        StreamReader sr = new StreamReader(openFileDialog1.FileName,Encoding.GetEncoding(1251));
+          textBox1.Text = sr.ReadToEnd();
+        sr.Close();
+      } 
+    }
+
+    private void кодироватьToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+
+      byte[] bstr;
+      
+        var encoding = Encoding.GetEncoding(1251);
+        bstr=Encoding.Default.GetBytes(textBox1.Text);
+      for (int i = 0; i < bstr.Length; i++)
+      {
+          textBox2.Text = textBox2.Text + " " + System.Convert.ToString(bstr[i],2) ;
+      }
+      }
+
+    private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+        saveFileDialog1.Filter = "Text Files|*.txt";
+        saveFileDialog1.FilterIndex = 2;
+        saveFileDialog1.RestoreDirectory = true;
+        if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+        {
+            File.WriteAllText(saveFileDialog1.FileName, textBox2.Text, Encoding.GetEncoding(1251));
+        }
+    }
+
+    private void декодироватьToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        byte[] bstr;
+        char[] ch;
+        string[] text;
+        int k = 0;
+        ch = textBox2.Text.ToCharArray();
+        for (int i = 0; i < textBox2.Text.Length; i++)
+        {
+            if (ch[i] == ' ')
+                k++;
+        }
+        k++;
+        text = new string[k];
+        bstr = new byte[k];
+        k = 0;
+        for (int i = 0; i < textBox2.Text.Length; i++)
+        {
+            if (ch[i] != ' ')
+            {
+                text[k] = text[k] + ch[i].ToString();
+                continue;
+            }
+            k++;
+        }
+
+      for (int i = 0; i < k + 1; i++)
+      {
+           bstr[i] = Convert.ToByte(text[i], 2);
+       }
+        char[] ch1;
+        Encoding myencoding;
+        myencoding = Encoding.GetEncoding(1251);
+        ch1 = myencoding.GetChars(bstr);
+        for (int i = 0; i < k + 1; i++)
+        {
+            textBox3.Text = textBox3.Text + ch1[i].ToString();
+        }
+    }
+
+    private void переводСтрокиMACToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        byte[] bstr;
+
+        var encoding = Encoding.GetEncoding(1251);
+        bstr = Encoding.Default.GetBytes(textBox1.Text);
+        for (int i = 0; i < bstr.Length; i++)
+        {
+            textBox5.Text = textBox5.Text + " " + bstr[i];
+        }
+            for (int i = 0; i < bstr.Length; i++)
+        {
+            if (bstr[i] == 10)
+            {
+                bstr[i] = bstr[i + 1];
+                i = i + 1;
+            }
+            textBox4.Text = textBox4.Text + " " + bstr[i];          
+        }
+
+        
+        /*for (int i = 0; i < bstr.Length; i++)
+        {
+            textBox4.Text = textBox4.Text + " " + bstr[i];
+        }*/
+    }
+
+    }
+  }
+
